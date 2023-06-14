383fa395c3d08a2645b9dc47fd0cab5e784136da
IuxMorti <zdracte21@bk.ru>
2023-06-14 16:28:14 +0500

    fix commit

--- api/comments/routers.py
+++ api/comments/routers.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.auth.routers import fastapi_users
-from api.comments.schemes import CreateComment
+from api.comments.schemes import CreateComment, CommentRead
 from db.models import Comment, User, Video
 from db.session import get_async_session
 
@@ -17,8 +17,9 @@ comments_api = APIRouter(
 
 @comments_api.get("/for_video/{video_id}")
 async def get_comments(video_id: uuid.UUID,
-                       db: AsyncSession = Depends(get_async_session)) -> list[Comment]:
-    return (await db.execute(select(Comment).where(Comment.video_id == video_id))).scalars().all()
+                       db: AsyncSession = Depends(get_async_session)) -> list[CommentRead]:
+    return [comment_to_commentRead(comment)
+            for comment in (await db.execute(select(Comment).where(Comment.video_id == video_id))).scalars().all()]
 
 
 @comments_api.post("/for_video/{video_id}", status_code=201)
@@ -32,3 +33,11 @@ async def add_comment(video_id: uuid.UUID,
 
     db.add(Comment(user_id=user.id, video_id=comment.video_id, text=comment.text))
     await db.commit()
+
+
+def comment_to_commentRead(comment: Comment) -> CommentRead:
+    return CommentRead(id=comment.id,
+                       text=comment.text,
+                       video_id=comment.video_id,
+                       user_id=comment.user_id,
+                       published_date=comment.published_date)
--- api/comments/schemes.py
+++ api/comments/schemes.py
@@ -1,5 +1,15 @@
 import uuid
+from datetime import datetime
+
 from pydantic import BaseModel
+
+
+class CommentRead(BaseModel):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    video_id: uuid.UUID
+    text: str
+    published_date: datetime
 
 
 class CreateComment(BaseModel):
