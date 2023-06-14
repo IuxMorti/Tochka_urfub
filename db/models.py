524e1c8652505168b3bfcd4c75c26ff4dfcbdefc
IuxMorti <zdracte21@bk.ru>
2023-06-14 17:44:22 +0500

    fix foreign key

--- db/models.py
+++ db/models.py
@@ -4,7 +4,7 @@ import uuid
 import sqlalchemy as alchemy
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
@@ -16,6 +16,15 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     url_photo: Mapped[str] = mapped_column(alchemy.String, nullable=True)
     username: Mapped[str] = mapped_column(alchemy.String(length=127), nullable=False)
     register_date: Mapped[datetime.datetime] = mapped_column(alchemy.TIMESTAMP, default=datetime.datetime.utcnow)
+    videos = relationship("Video",
+                          back_populates="User",
+                          cascade="all, delete",
+                          passive_deletes=True, )
+
+    comments = relationship("Comment",
+                            back_populates="User",
+                            cascade="all, delete",
+                            passive_deletes=True, )
 
 
 class Video(Base):
@@ -27,13 +36,16 @@ class Video(Base):
     is_private: Mapped[bool] = mapped_column(alchemy.Boolean, default=False)
     preview: Mapped[str] = mapped_column(alchemy.String, nullable=True)
     published_date: Mapped[datetime.datetime] = mapped_column(alchemy.TIMESTAMP, default=datetime.datetime.utcnow)
-    owner_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), nullable=False)
-
+    owner_id: Mapped[uuid.UUID] = mapped_column(alchemy.UUID, ForeignKey("user.id",  ondelete="CASCADE"), nullable=False)
+    comments = relationship("Comment",
+                            back_populates="Video",
+                            cascade="all, delete",
+                            passive_deletes=True, )
 
 class Comment(Base):
     __tablename__ = "comment"
     id: Mapped[uuid.UUID] = mapped_column(alchemy.UUID, primary_key=True, default=uuid.uuid4)
-    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), nullable=False)
-    video_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("video.id"), nullable=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id",  ondelete="CASCADE"), nullable=False)
+    video_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("video.id",  ondelete="CASCADE"), nullable=False)
     text: Mapped[str] = mapped_column(alchemy.String(511), nullable=False)
     published_date: Mapped[datetime.datetime] = mapped_column(alchemy.TIMESTAMP, default=datetime.datetime.utcnow)
